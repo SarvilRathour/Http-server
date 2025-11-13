2ad29ca772c2ce993b547215e91c43ea0a41df43
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-11-13 23:25:21 +0530

    extracting url path

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 #[allow(unused_imports)]
 use std::net::{TcpListener,TcpStream};
-use std::io::Write;
-use std::io::{BufRead,BufReader};
+use std::io::prelude::*;
+use std::io::{self, BufRead as _, BufReader, Write};
+fn handle_client(mut stream:&TcpStream)->io::Result<()>{
+     let mut reader=BufReader::new(stream);
+     let mut url_path=String::new();
+     reader.read_line(&mut url_path)?;
+     println!("{}",url_path);
+     url_path = url_path.trim().to_string();
+     match url_path.as_str(){
+          "GET /abcdefg HTTP/1.1"=>stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n"),
+          "GET / HTTP/1.1"=>stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n"),
+          _=>stream.write_all(b"HTTP/1.1 400 BAD REQUEST\r\n\r\n"),
+     };
+     Ok(())
+}
 fn main() {
-    println!("Logs from your program will appear here!");
-
      let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
      for stream in listener.incoming() {
          let mut stream=stream.unwrap();
-         let mut reader=BufReader::new(&stream);
-         let request=reader.lines().next().unwrap().unwrap();
-         
-         stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
-         //
+         handle_client(&stream);
+         // stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
      }
      
 }
