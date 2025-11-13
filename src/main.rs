294cb8e5423f4eb1007fe7baba5f57c70f6885f7
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-11-13 17:35:36 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[allow(unused_imports)]
+use std::net::{TcpListener,TcpStream};
+use std::io::Write;
+use std::io::{BufRead,BufReader};
+fn main() {
+    println!("Logs from your program will appear here!");
+
+     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    
+     for stream in listener.incoming() {
+         let mut stream=stream.unwrap();
+         let mut reader=BufReader::new(&stream);
+         let request=reader.lines().next().unwrap().unwrap();
+         
+         stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+     }
+     
+}
